232ec7a3d03e3c5882814adf22da02c6a9846e50
Simon <sacost6@uic.edu>
2026-03-05 10:14:17 -0600

    Add project files.

---
+++ IngredientDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeCost.Shared;
+
+public class IngredientDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string BaseUnit { get; set; } = "Gram"; // e.g., Gram, oz, cup
+    public decimal CostPerBaseUnit { get; set; }
+}
